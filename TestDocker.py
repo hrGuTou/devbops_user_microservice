4d4b92d462d02029fe9f7cc2c593fc00597a9e07
Haoran He <hrgutou@gmail.com>
2021-02-05 00:59:22 -0500

    fix unit test

--- TestDocker.py
+++ TestDocker.py
@@ -13,7 +13,7 @@ class TestDocker(unittest.TestCase):
         }
         rv = requests.post('http://127.0.0.1:4040/login', json=req, headers=headers)
 
-        assert rv.status_code == 202
+        assert rv.status_code == 200
 
 if __name__ == '__main__':
     unittest.main()

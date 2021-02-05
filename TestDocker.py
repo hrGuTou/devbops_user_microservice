95a2b43799b747d3a1ea261e3ff3733bd8acfba9
Haoran He <hrgutou@gmail.com>
2021-02-05 00:32:34 -0500

    fix unit test

--- TestDocker.py
+++ TestDocker.py
@@ -8,7 +8,7 @@ class TestDocker(unittest.TestCase):
             "Username": "test",
             "Password": "test"
         }
-        rv = requests.post('http://localhost:8081/login', json=req)
+        rv = requests.post('http://127.0.0.1:4040/login', json=req)
 
         assert rv.status_code == 202
 
